27a5165d6f1b219ef3f91001a4c7962ab720a388
Serena <sharri23@uvm.edu>
2024-10-23 10:21:28 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function Monster(name, type, health, strength) {
+    this.name = name;
+    this.type = type;
+    this.health = health;
+    this.strength = strength;
+}
+
+function getrandomInt(min, max)
+return Math.floor(Math.random() * (max - min + 1))
+{
+    const ghost = new Monster ('Ghost', 'Spirit', 0, 0);
+    const whitch = new Monster ('Witch', 'Creature', 0, 0)
+    const vampire = new Monster ('Vampire, Creature', 0, 0)
+}
+
+function displayMonsters() {
+    ghost.health = getRandomInt (20,100);
+    ghost.strength = getRandomInt (20,100)
+}
